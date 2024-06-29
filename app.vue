ea84a0d660ca338f6e5e885f671663cbb9e9234b
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-29 14:28:43 +0530

    first initialization push along with navbar basic and body image

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <!-- <NuxtWelcome /> -->
+    <!-- Navbar -->
+
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"> 
+      <a  class ="text-3xl font-mono" href="">CarTrader</a>
+    </header>
+
+    <!-- Navbar -->
+
+    <!-- home hero -->
+    <div class="relative h-screen w-full bg-no-repeat bg-cover bg-center" style="background-image: url('https://carconfigurator.ferrari.com/rt-assets/data/cars/sf90stradale/ui/splashpage.jpg');">
+  <div class="z-10 absolute inset-0 flex justify-center items-center">
+    <div class="text-center w-full px-4 sm:w-1/2">
+      <h1 class="text-6xl sm:text-8xl font-serif mb-10 text-white">Find your drive</h1>
+    </div>
+  </div>
+  <!-- give a little dark tint  -->
+  <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>  
+</div>
+
+    <!-- home hero -->
+  </div>
+</template>
