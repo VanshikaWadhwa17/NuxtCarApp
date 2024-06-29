3b3ec565eb4a4e8c82fc0d99b85899e0033a403e
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-29 14:45:26 +0530

    added search bar and search button

--- app.vue
+++ app.vue
@@ -15,6 +15,14 @@
   <div class="z-10 absolute inset-0 flex justify-center items-center">
     <div class="text-center w-full px-4 sm:w-1/2">
       <h1 class="text-6xl sm:text-8xl font-serif mb-10 text-white">Find your drive</h1>
+
+      <!-- home search bar -->
+      <div class="font-serif w-[700px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+        <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="search by city..."/>
+        <button class="bg-sky-500 px-10 text-white">Search</button>
+      </div>
+      <!-- home search bar -->
+
     </div>
   </div>
   <!-- give a little dark tint  -->
