06f4540f8e6c3c73578e94c6c8804cfe7eb63cad
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-30 21:48:27 +0530

    adding /homepage routing for the app

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+       <!-- Navbar -->
+    
+       <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"> 
+          <a  class ="text-3xl font-mono" href="">CarTrader</a>
+        </header>
+    
+        <!-- Navbar -->
+  
+    </div> 
+    
+  </template>
+  
