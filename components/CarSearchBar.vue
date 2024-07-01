39bf9e91f693800fc78fab273632c4da44af14b5
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-07-01 18:19:51 +0530

    added programmatic navigation in CarSearchBar.vue page

--- components/CarSearchBar.vue
+++ components/CarSearchBar.vue
@@ -1,8 +1,30 @@
+<script setup>
+import { navigateTo } from '#app';
+
+
+const city=ref("")
+const cityError=ref(false)
+
+// const router=useRouter()
+// const handleSearch=()=>{
+//   router.push(`/city/${city.value}/car`)
+// }
+
+// Nuxt doesnt need to use this all it can simply do it with navigateTo() fxn:
+const handleSearch=()=>{
+  if(!city.value){
+    return  cityError.value=true 
+    //if city.value doesnt exist come out of the fxn - if this is not used it goes to /city//car- 404page
+  }
+  navigateTo(`/city/${city.value}/car`)
+}
+</script>
 <template>
     <!-- home search bar -->
     <div class="font-serif w-[700px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-          <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="search by city..."/>
-          <button class="bg-sky-500 px-10 text-white">Search</button>
+          <input v-model="city" type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="search by city..." :class="cityError ?'border-red-500 border':''">
+
+          <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
         </div>
         <!-- home search bar -->
 </template>
