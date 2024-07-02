629b299543db3e5959f03af56a7b8de4de1a258a
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-07-02 19:46:01 +0530

    implemented nested pages with the help of <NuxtPage/> component

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,19 +1,6 @@
 <template>
     <div>
-       <Navbar/>
-        <!-- cars page -->
-      <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-        <div class="mt-32 flex">
-          <CarSideBar/>
-          <!-- car side bar -->
-  
           <CarCards/>
-        </div>
-      </div>
-        <!-- cars page -->
     </div>
 </template>
 
-<!-- /city/:city/car/:make 
-or 
-/city/:city/car -->
